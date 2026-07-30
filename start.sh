02f5fa10a67318704b6a52e0f6ed30fcb08a3cdf
ericksacatoro <erick.sacatoro9157@utc.edu.ec>
2026-07-30 00:36:42 -0500

    Configuracion

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-python manage.py migrate --noinput
+python manage.py migrate Helpdesk 0004_alter_customer_options_alter_equipment_options_and_more --fake
+python manage.py migrate
+python manage.py createsuperuser --noinput
 gunicorn SoporteTI.wsgi:application --bind 0.0.0.0:$PORT
