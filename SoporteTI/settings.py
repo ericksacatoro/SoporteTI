1c931d55ab0d365500d6c43f3bec4b5de6b8f392
ericksacatoro <erick.sacatoro9157@utc.edu.ec>
2026-07-30 00:44:16 -0500

    ...

--- SoporteTI/settings.py
+++ SoporteTI/settings.py
@@ -22,12 +22,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'SoporteTI.urls'
@@ -91,6 +93,8 @@ LOGOUT_REDIRECT_URL= '/login/'
 STATIC_URL  = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'SoporteTI/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Archivos media (adjuntos)
 MEDIA_URL  = '/media/'
--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+# Recolectar archivos estáticos
+python manage.py collectstatic --noinput
+
+# Migrar con --fake para la conflictiva
 python manage.py migrate Helpdesk 0004_alter_customer_options_alter_equipment_options_and_more --fake
 python manage.py migrate
-python manage.py createsuperuser --noinput
+
+# Crear superusuario si no existe (usando variables de entorno)
+python manage.py createsuperuser --noinput 2>/dev/null || echo "Superuser already exists or no credentials provided"
+
+# Iniciar Gunicorn
 gunicorn SoporteTI.wsgi:application --bind 0.0.0.0:$PORT
