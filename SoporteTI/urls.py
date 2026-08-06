d85ebced88724bd1ac30d2dc32b61b57d8515c55
ericksacatoro <erick.sacatoro9157@utc.edu.ec>
2026-08-05 19:49:31 -0500

    Reparando el manifest

--- SoporteTI/urls.py
+++ SoporteTI/urls.py
@@ -3,9 +3,22 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+import os
+
+def serviceworker(request):
+    sw_path = os.path.join(settings.BASE_DIR, 'SoporteTI', 'static', 'serviceworker.js')
+    with open(sw_path, 'r') as f:
+        content = f.read()
+    response = HttpResponse(content, content_type='application/javascript')
+    response['Service-Worker-Allowed'] = '/'
+    return response
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Service Worker en la raíz (necesario para scope /)
+    path('serviceworker.js', serviceworker, name='serviceworker'),
 
     # Login nativo de Django con plantilla personalizada
     path('login/', auth_views.LoginView.as_view(
